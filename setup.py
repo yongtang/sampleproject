362aedbc2c71a90baa5d3453214321f905736c75
Yong Tang <yong.tang.github@outlook.com>
2020-08-16 08:58:56 -0700

    Setup GitHub CI
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

---
+++ setup.py
@@ -0,0 +1,48 @@
+# Copyright 2020 Yong Tang. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Setup for pip package."""
+
+import os
+import sys
+import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+project = "fluyt"
+version = "0.1.0"
+if "--nightly" in sys.argv:
+    nightly_idx = sys.argv.index("--nightly")
+
+    project = project + "-nightly"
+    version = version + ".dev" + sys.argv[nightly_idx + 1]
+
+    sys.argv.remove("--nightly")
+    sys.argv.pop(nightly_idx)
+
+
+class BinaryDistribution(setuptools.dist.Distribution):
+    def has_ext_modules(self):
+        return True
+
+
+setuptools.setup(
+    name=project,
+    version=version,
+    packages=setuptools.find_packages(where=".", exclude=["tests"]),
+    python_requires=">=3.6, <3.9",
+    install_requires=["tensorflow"],
+    zip_safe=False,
+    distclass=BinaryDistribution,
+)
